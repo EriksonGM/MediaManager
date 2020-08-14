1391db28e648c08095bdb7eee34f34a662cf1cf7
EriksonGM <eriksonlove@gmail.com>
2020-08-14 23:43:05 +0100

    Add project files.

---
+++ src/MediaManager.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MediaManager.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
